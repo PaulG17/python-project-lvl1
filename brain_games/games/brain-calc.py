5711a7351a10732dd892860b2d2b19b1d4dd4278
Paul K <gg1779@gmail.com>
2020-10-16 16:50:37 +0300

    stage6

--- brain_games/games/brain-calc.py
+++ brain_games/games/brain-calc.py
@@ -1,43 +1,42 @@
 import prompt
+import random
 from brain_games.games.engine import round_number
-
 
 
 def calc():
     print("Welcome to the Brain Games!")
     user_name = prompt.string("May I have your name? ")
     print(f"Hello, {user_name}!")
-    print('Answer "yes" if the randomber is even, otherwise answer "no".')
+    print('What is the result of the expression?')
 
     i = 0
     count = 0
-    while i != 3 or count <= round_number:
-        count += 1
-        first_num = random_num
-        second_num = random_num
-        action = random_num
+    while i != 3 or count < round_number:
+        first_num = random.randint(1, 100)
+        second_num = random.randint(1, 100)
+        random_three = random.randint(1, 100)
 
+        count += 0
         result = 0
 
-        if action < 33:
+        if random_three < 33:
             random_num = (f"{first_num} + {second_num}")
             result = first_num + second_num
-        elif action > 33 and action < 66:
+        elif random_three > 33 and random_three < 66:
             random_num = (f"{first_num} - {second_num}")
             result = first_num - second_num
-        elif action > 66:
+        elif random_three > 66:
             random_num = (f"{first_num} * {second_num}")
             result = first_num * second_num
 
-
         print(f"Question: {random_num}")
-        yesno = prompt.string("Your answer: ")
+        answer = int(prompt.string("Your answer: "))
 
-        if yesno == result:
+        if answer == result:
             print('Correct!')
             i += 1
         else:
-            print(f"'{yesno}' is wrong answer ;(. "
+            print(f"'{answer}' is wrong answer ;(. "
                   f"Correct answer was '{result}'.")
             print(f"Let's try again, {user_name}")
     print(f'Congratulations, {user_name}!')

4a21f766b7988c20391cc1344520c3926a5acb17
Paul K <gg1779@gmail.com>
2020-11-11 19:13:57 +0300

    action->sign

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -9,8 +9,8 @@ def prepare_round():
     one = randint(1, 100)
     two = randint(1, 100)
     operations = [("+", add), ("*", mul), ("-", sub)]
-    action, operation = choice(operations)
-    question = f"{one} {action} {two}"
+    sign, operation = choice(operations)
+    question = f"{one} {sign} {two}"
     right_answer = operation(one, two)
 
     return question, str(right_answer)
